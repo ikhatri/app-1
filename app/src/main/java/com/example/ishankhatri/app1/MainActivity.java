cbd9e832623dbdd301d18976c65ddbfad26e6f18
Ishan Khatri <ikhatrix@gmail.com>
2016-07-31 11:33:52 -0400

    Made a toast

--- app/src/main/java/com/example/ishankhatri/app1/MainActivity.java
+++ app/src/main/java/com/example/ishankhatri/app1/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.ishankhatri.app1;
 
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,19 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toast.makeText(MainActivity.this,"Message sent to Timbuktu",Toast.LENGTH_LONG).show();
     }
 
     //Send Message method
-    public void sendMessage(View view){
+    /*public void sendMessage(View view){
         //Do something when the button is pressed
-        // 1. Instantiate an AlertDialog.Builder with its constructor
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        Toast.makeText(MainActivity.this,"Message sent to Timbuktu",Toast.LENGTH_LONG).show();
+    }*/
 
-        // 2. Chain together various setter methods to set the dialog characteristics
-        builder.setMessage(R.string.app_name)
-                .setTitle(R.string.app_name);
-
-        // 3. Get the AlertDialog from create()
-        AlertDialog dialog = builder.create();
-    }
+    Button button = (Button) findViewById(R.id.send_button);
+    View.OnClickListener myListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+            Toast.makeText(MainActivity.this,"Message sent to Timbuktu",Toast.LENGTH_LONG).show();
+        }
+    };
+    button.setOnClickListener(myListener);
 }
