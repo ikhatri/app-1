cbb8142d3b3e9840a9c4e48922410048d6ed7725
Ishan Khatri <ikhatrix@gmail.com>
2016-07-31 10:25:31 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/ishankhatri/app1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ishankhatri.app1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Send Message method
+    public void sendMessage(View view){
+        //Do something when the button is pressed
+        Intent lmao = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        getIntent().putExtra(EXTRA_MESSAGE, message);
+        startActivity(lmao);
+    }
+}
