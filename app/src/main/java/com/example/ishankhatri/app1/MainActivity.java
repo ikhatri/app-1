8cfe4c191dc127d0be0098ac6ffb6645b3deab35
Ishan Khatri <ikhatrix@gmail.com>
2016-07-31 23:07:20 -0400

    Save message to a file, and then retrieve the message & read it. Buffer is arbitarily set to 1024

--- app/src/main/java/com/example/ishankhatri/app1/MainActivity.java
+++ app/src/main/java/com/example/ishankhatri/app1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ishankhatri.app1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
@@ -10,33 +11,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
 
+    //Constants
+    //Key for a string stored in an intent
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    //Name of a file where data is stored
+    public final static String FILENAME = "hello_file";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toast.makeText(MainActivity.this,"Message sent to Timbuktu",Toast.LENGTH_LONG).show();
-        /*Button button = (Button) findViewById(R.id.send_button);
-        View.OnClickListener myListener = new View.OnClickListener() {
-            public void onClick(View v) {
-                // Do something in response to button click
-
-            }
-        };
-        button.setOnClickListener(myListener);*/
     }
 
     //Send Message method
-    public void sendMessage(View view){
+    public void saveMessage(View view) throws IOException{
         //Do something when the button is pressed
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
         EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        fos.write(editText.getText().toString().getBytes());
+        fos.close();
         Toast.makeText(MainActivity.this,"Message sent to Timbuktu",Toast.LENGTH_LONG).show();
+    }
+
+    public void openMessage(View view) throws IOException {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        FileInputStream fis = openFileInput(FILENAME);
+        byte[]b=new byte[1024];
+        fis.read(b);
+        String message= new String(b);
+        fis.close();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
